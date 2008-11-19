2f8c502dfdcb022e16b341c658c4b5ac00434859
yeli <yeli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-19 15:53:55 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmbugs@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/glmmBUGS/R/getRaggedSeq.R
+++ pkg/glmmBUGS/R/getRaggedSeq.R
@@ -6,6 +6,8 @@ function(data) {
   result = cumsum(freq)
   result = c(start=1, 1+result)
   names(result) = c(names(result)[-1], "end")
+  # get rid of trailing white space
+  names(result) = gsub("[[:space:]]+$", "", names(result))
   return(result)
 }
 
