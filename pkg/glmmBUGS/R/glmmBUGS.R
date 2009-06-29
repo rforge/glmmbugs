0011fe13e5e9a2ca5d6e129ce4447f2cceca2793
lutong <lutong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-29 18:16:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmbugs@139 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/glmmBUGS/R/glmmBUGS.R
+++ pkg/glmmBUGS/R/glmmBUGS.R
@@ -1,80 +1,39 @@
-`glmmBUGS` <-
-function(formula, data, effects, 
-  modelFile = "model.bug", initFile = "getInits.R", 
-  family=c("bernoulli", "binomial", "poisson", "gaussian"),
-spatial=NULL, spatialEffect = NULL, reparam=NULL, prefix=NULL) {
+glmmBUGS <- function (formula, data, effects, modelFile = "model.bug", initFile = "getInits.R", family = c("bernoulli", "binomial", "poisson", "gaussian"), 
+    spatial = NULL, spatialEffect = NULL, reparam = NULL) 
+{
 
-# reparam = list(CSDUID = c(20000, 10), observations=50 )
-
-  family = family[1]
-  if(!is.character(family)) {
-    warning("family must be a character string, ie \"poisson\" or \"binomial\" ")
-  }
-
-
-
-
-# get design matrix
-data = getDesignMatrix(formula, data, effects)
-
-covariates = attributes(data)$covariates
-observations = attributes(data)$response
-
-
-# get rid of rows where there are NA's in covariates
-# but NA's in response are ok.
-if(length(covariates)) {
-  data = data[!apply(data[,unlist(covariates), drop=F], 1, 
-    function(qq) any(is.na(qq))), ]
-}
-
-
- # create the ragged array
-ragged = winBugsRaggedArray(data, effects =effects,
-  covariates = covariates, observations = observations)
-
-#add spatial to the ragged array
-if(!is.null(spatial))
-	ragged = addSpatial(spatial, ragged, spatialEffect)
-
-# run pql model
-
-# get rid of missing values
-dataForPQL = na.omit(data)
-
-thepql = glmmPQLstrings(effects=effects, covariates = covariates, 
-  observations = observations, data=dataForPQL, family=family)
- 
-# extract properly formatted starting values from the pql
-startingValues = getStartingValues(pql=thepql, ragged=ragged) 
-
-# if reparametrizing, subtract stuff from starting values
-if(!is.null(reparam)) {
-
-# and add stuff to the ragged array
- for(D in names(reparam)) {
-
-    ragged[[paste("X", D, "reparam", prefix, sep="")]] = reparam[[D]]
-
-    startingValues$intercept = startingValues$intercept - 
-      reparam[[D]]%*%startingValues[[paste("beta", D, "reparam", sep="")]]
- }
-
-}
-attributes(ragged)$covariates = covariates
-
-# write a function to generate starting values
-startingFunction(startingValues, initFile)
-
-# create the model
-spatialEffect = grep("^N[[:alnum:]]+Spatial$", names(ragged), value=T)
-spatialEffect = gsub("^N", "", spatialEffect)
-spatialEffect = gsub("Spatial$", "", spatialEffect)
-
-writeBugsModel(modelFile, effects =effects,
-    covariates = covariates, observations = observations,
-     family=family, spatial = spatialEffect, reparam = names(reparam), prefix=prefix ) # add spatial            
+    data = getDesignMatrix(formula, data, effects)
+    data = na.omit(data)
+    covariates = attributes(data)$covariates
+    observations = attributes(data)$response
+    
+     #if new col created, change it back for ragged
      
-return(list(ragged=ragged, startingValues = startingValues, pql=thepql))  
-}
+     
+#        reparam = paste(prefix, reparam, sep="")
+     
+    ragged = winBugsRaggedArray(data, effects = effects, covariates = covariates, 
+        observations = observations, prefix= prefix , reparam=reparam)  
+    #ragged$Xreparam = ????
+    if (!is.null(spatial)) 
+        ragged = addSpatial(spatial, ragged, spatialEffect, prefix= prefix)
+    thepql = glmmPQLstrings(effects = effects, covariates = covariates, 
+        observations = observations, data = data, family = family)
+    startingValues = getStartingValues(pql = thepql, ragged = ragged, prefix=prefix, reparam = reparam )
+    
+    startingFunction(startingValues, file="getInits.R")
 
+    spatialEffect = grep("^N[[:graph:]]+Spatial$", names(ragged), 
+        value = T)
+    spatialEffect = gsub("^N", "", spatialEffect)
+    spatialEffect = gsub("Spatial$", "", spatialEffect)
+    
+    effects = paste(prefix, effects, sep="")
+
+    
+    writeBugsModel(file="model.bug", effects = effects, covariates = covariates, 
+        observations = observations, family = family, spatial = spatialEffect,
+        prefix= attributes(ragged)$prefix, reparam =reparam)
+         return(list(ragged = ragged, startingValues = startingValues, 
+        pql = thepql))
+}
