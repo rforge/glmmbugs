57bd3805cd042d644ccf8a8a0d2048315aba8061
lutong <lutong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-29 15:23:01 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmbugs@189 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- docs/smoking/smoking.R
+++ docs/smoking/smoking.R
@@ -14,6 +14,10 @@ library(spdep)
 # just grab 1 region: EA_or_DA = 35230258
 hamiltoncchs = popdata[popdata$CD_sub=="3525005",]
 hamiltonSpatial = adjdata01[adjdata01$CSDUID == "3525005",]
+
+# change directory to billy:
+#cchsSpatial = readShapePoly(fn="da2001_HamiltonCSD")
+
 library(spdep)
 adj01 = poly2nb(hamiltonSpatial, as.character(hamiltonSpatial$DAUID))
 
@@ -47,34 +51,55 @@ spatial = adj01
 spatialEffect = "EA_or_DA"
 family="bernoulli"
 
-formula = SMK_01a ~ age:sex
+
+#formula = SMK_01a ~ age:sex
 # formula for quadratic terms: 
 
-formula = SMK_01a ~ (age+agesq):sex
+#formula = SMK_01a ~ (age+agesq):sex
+
+prefix=NULL
+mypriors = NULL
+
+#c("SDEA_or_DA" = "dunif(0, 100)", "SDEA_or_DASpatial"="dunif(0, 100)", 
+#"betaobservations[2]" = "dunif(-10, 10)")
 
 
 # for males in the model only: 
-data =hamiltoncchsm
-#formula = SMK_01a ~ age+agesq
-reparam =list(c(0.14, 0.32, 0.27, 0.13, 0.05))
-names(reparam)= "observations"
-prefix=NULL
-mypriors = c("SDEA_or_DA" = "dunif(0, 25)", "SDEA_or_DASpatial"="dunif(0, 100)")
-
-
-ageRange = range(hamiltoncchsm$age)
 
 #internalKnots = c(6, 10, 14)  # 3 internal knots
-internalKnots = c(32, 52)  # 3 internal knots
+internalKnots = 25 #c(32, 52)  # 3 internal knots
+splineDegree=2
+ageRange = range(hamiltoncchsm$age)
 
 #Get basis for cubic splines, 4+3-1(intercept) = 6
 library(splines)
-thesplines = as.matrix(bs(hamiltoncchsm$age, knots=internalKnots, Boundary.knots=ageRange))
+
+bsLast = function(x, df=NULL, knots=NULL, degree=3, Boundary.knots=range(x)) {
+  fromBs = bs(x, df, knots, degree, Boundary.knots, intercept=T)
+  fromBs[,-dim(fromBs)[2] ]
+}
+
+
+#formula = SMK_01a ~ age+agesq
+reparam = list(c(as.vector(bsLast(50, knots=internalKnots, Boundary.knots=ageRange, degree=splineDegree))))
+
+#reparam =list(c(0.53, 0.39, 0.036, 0, 0))
+names(reparam)= "observations"
+
+thesplines = as.matrix(bsLast(hamiltoncchsm$age, 
+  knots=internalKnots, Boundary.knots=ageRange, 
+    degree=splineDegree))
+
 colnames(thesplines) = paste("s", colnames(thesplines), sep="")   
 class(thesplines)="matrix"
-hamiltoncchsm = cbind(hamiltoncchsm, thesplines) 
 
-formula = SMK_01a ~ s1+ s2+ s3 + s4 + s5
+
+hamiltoncchsm = cbind(hamiltoncchsm, thesplines) 
+data =hamiltoncchsm
+
+formula = as.formula(paste("SMK_01a ~ ", paste(colnames(thesplines), collapse="+")))
+
+#formula = SMK_01a ~ s1+ s2+ s3 + s4 + s5
 #hamiltoncchsm$agebs = bs(hamiltoncchsm$age, knots = c(32,52))
 #hamiltoncchsm$agesqbs = bs(hamiltoncchsm$agesq, knots = c(3000, 4500))
 
@@ -119,25 +144,71 @@ formula = SMK_01a ~ s1+ s2+ s3 + s4 + s5
 
        smkResult = bugs(ragged, getInits, 
        parameters.to.save = names(getInits()),
-       model.file="model.bug", n.chain=3, n.iter=50, n.burnin=10, n.thin=2,
+       model.file="model.bug", n.chain=3, n.iter=200000, n.burnin=10000, n.thin=100,
        program="WinBUGS", debug=T)     
 
 #popdata$ageCut = cut(popdata$age, c(0, seq(20, 60, by=5), Inf))
+
+source("C:\\Documents and Settings\\luzhou\\My Documents\\glmmBUGS\\pkg\\glmmBUGS\\R\\restoreParams.R")
 smkParams = restoreParams(smkResult, ragged)
+t(apply(smkParams$betas, 3, function(qq) c(mean=mean(exp(qq)), probGTone=mean(qq>0), quantile(exp(qq), c(0.025, 0.975)))))
+
 
 smkSummary = summaryChain(smkParams)
+checkChain(smkParams)
 source("C:\\Documents and Settings\\luzhou\\My Documents\\newDiseaseMapping\\pkg\\diseasemapping\\R\\mergeBugsData.R")
 
-smk = mergeBugsData(hamiltonSpatial, smkSummary, by.x= "DAUID")
+smksp = mergeBugsData(hamiltonSpatial, smkSummary, by.x= "DAUID")
+
+#for cchs: 
+cchssp = mergeBugsData(cchsSpatial, smkSummary, by.x="DA2001")
+
+# exceed probability plot: 
+exceed =  apply(smkParams$REA_or_DA, 3, function(qq) mean(qq > log(1.1)))
+pop = mergeBugsData(smksp, exceed, "DAUID", newcol="prob10")
+exceed =  apply(smkParams$REA_or_DA, 3, function(qq) mean(qq > log(1.2)))
+pop = mergeBugsData(smksp, exceed, "DAUID", newcol="prob20")
+exceed =  apply(smkParams$REA_or_DA, 3, function(qq) mean(qq > log(1.5)))
+pop = mergeBugsData(smksp, exceed, "DAUID", newcol="prob50")
+
+spplot(pop, "prob10", lty=0)
+
+# do the fitted plot using the color control according to the slides:
+
+Ncol = 7
+library(classInt)
+ci=classIntervals(smksp$FittedRateEA_or_DA.mean, Ncol, style="kmeans")
+library(RColorBrewer)
+colours = brewer.pal(Ncol, "YlOrRd")
+colFac = findColours(ci, colours)
+ontarioBox = list(xlim=c(-83, -73.72315), ylim=c(41.8, 48))
+print(plot(smksp, col=colFac, lwd=0.2) )
+legend("topright", fill=attr(colFac, "palette"), legend=names(attr(colFac, "table")), bg="white") 
+
+
+
+# do matplot: 
+#basisW<-matrix(poster[,,13:18], ncol=6, dimnames=list(NULL, c("s1W","s2W","s3W","s4W"
+#,"s5W","s6W")))
+
+b <- grep("^s", dimnames(smkParams$betas)[[3]], value=T)
+bCoef= matrix(smkParams$betas[,,b], ncol=length(b), dimnames=list(NULL, b))
+
+ageSeq = seq(min(hamiltoncchsm$age), max(hamiltoncchsm$age), by=0.1)
+
+splinesToPlot = as.matrix(bsLast(ageSeq, knots=internalKnots, 
+  Boundary.knots=ageRange, degree=splineDegree))
+
+smkpredict = splinesToPlot %*% t(bCoef)
+smkMean = apply(smkpredict, 1, mean)
+smkQuantile = apply(smkpredict, 1, function(qq) quantile(qq, c(0.025, 0.975)))
 
 
 
 
-
-
-
-
-
+matplot(ageSeq, smkQuantile[1,],  xlim =c(12, 95), ylim = c(-0.8, 6.5), xlab= "age", ylab= "smk", type="l", lty=1, col="green", main="smoking against age")
+matlines(ageSeq, smkQuantile[2,], lty=1, col="green")
+matlines(ageSeq, smkMean, lty=1)
 
 
 
