074de1cb0a5e955cd316609d60be9253a792f6a2
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-19 20:46:40 +0000

    fixed bug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmbugs@254 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/glmmBUGS/R/checkChain.R
+++ pkg/glmmBUGS/R/checkChain.R
@@ -1,46 +1,64 @@
+plotOne = function(mat, main=NULL) {
+	matplot(mat, lty=1, type="l",  ylab=main)
+}
+
 checkChain = function(chain, parameters=NULL, oneFigure=TRUE) {
 
-	if(is.array(chain))
+if(is.array(chain)) {
   chain = list(beta=chain)
+}
 
 if(is.null(parameters)) {
 	parameters = grep("^sd|range|intercept|betas",
 			names(chain),value=TRUE,ignore.case=TRUE)
 }
-
-if(!all(parameters %in% names(chain)))
-	warning("parameter ", parameters[!parameters %in% names(chain)], 
-			"not found")
-
   # find out the number of parameters
   	scalars = unlist(lapply(chain[parameters], is.matrix))
 	notScalars = parameters[!scalars]
 	scalars = parameters[scalars]
-
+	betas = NULL
+	if(length(notScalars) & any(names(chain)=="betas"))  {
+		betas = notScalars[
+				notScalars %in% dimnames(chain$betas)[[3]] 
+						]
+	}
+	parArray = notScalars[
+			unlist(lapply(chain[notScalars], 
+					function(qq) length(dim(qq))==3)
+		)
+	]
+	
 
 	
 	if(oneFigure) {
-		Nplots = length(scalars)
+		Nplots = length(scalars) + length(betas)
 
-		for(Dbeta in notScalars)
-			Nplots = Nplots + dim(chain[[Dbeta]])[3]
-
-	  par(mfrow=c(ceiling(Nplots/4),min(c(4, Nplots))))
+		for(Darray in parArray)
+			Nplots = Nplots + dim(chain[[Darray]])[3]
+	
+		if(Nplots==0) warning("Nothing to plot")
+		if(Nplots > 16) warning("Creating", Nplots, "plots, this might not work")
+		
+	  par(mfrow=c(ceiling(Nplots/4),min(c(4, Nplots))),
+			  mar=c(1.5,2.5,0,0), mgp=c(1.5, 0.5, 0))
   }
   
-  for(Dbeta in parameters) {
-	  if(Dbeta %in% scalars) {
-		  plotOne(chain[[Dbeta]], Dbeta)
-	  } else {
-		  for(D2 in dimnames(chain[[Dbeta]])[[3]])
-			  plotOne(chain[[Dbeta]][,,D2], 
-					  D2)
-		  }
-	 }
+  for(D in scalars)
+	  plotOne(chain[[D]], D)
+
+  for(D in betas)
+	  plotOne(chain$betas[,,D], D)
   
+  for(D in parArray){
+	  Dhere = chain[[D]]
+	  Shere = dimnames(Dhere)[[3]]
+	  if(is.null(Shere)) Shere = seq(1, dim(Dhere)[3])
+	  for(D2 in Shere) {
+		  plotOne(Dhere[,,D2], paste(D, ":", D2,sep=""))		 
+	  }
+  }
+	  
+	  
   invisible() 
 }
 
-plotOne = function(mat, main=NULL) {
- matplot(mat, lty=1, type="l", xlab='iteration', ylab=main)
-}
